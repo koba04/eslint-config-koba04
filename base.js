e941f1697ab128c2509c670a3c019ed32fa181ea
Toru Kobayashi <koba0004@gmail.com>
2016-01-18 19:40:46 +0900

    Fixed eslint errors

--- base.js
+++ base.js
@@ -1,16 +1,16 @@
 'use strict';
 
 module.exports = {
-  "env": {
-    "mocha": true
+  env: {
+    mocha: true,
   },
-  "rules": {
-    "camelcase": 0,
-    "id-length": 0,
-    "indent": [2, 2, {"SwitchCase": 1}],
-    "no-unused-vars": [2, {"args": "none"}],
-    "no-console": 0,
-    "no-alert": 0,
-    "object-curly-spacing": [2, "never"]
-  }
+  rules: {
+    camelcase: 0,
+    'id-length': 0,
+    indent: [2, 2, {SwitchCase: 1}],
+    'no-unused-vars': [2, {args: 'none'}],
+    'no-console': 0,
+    'no-alert': 0,
+    'object-curly-spacing': [2, 'never'],
+  },
 };
