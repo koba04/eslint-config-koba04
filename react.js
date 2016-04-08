beeacae1b21114b6463fe9f1e68c29e4969de2d7
Toru Kobayashi <koba0004@gmail.com>
2016-04-08 18:19:29 +0900

    Add a rule for React/JSX

---
+++ react.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'react/jsx-no-bind': [
+      2,
+      {
+        ignoreRefs: true,
+        allowArrowFunctions: true,
+        allowBind: false,
+      },
+    ],
+  },
+};
